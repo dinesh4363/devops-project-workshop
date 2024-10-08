c717f343d715677d6e9480b77777f116dfaf85c2
Dinesh Kumar <dineshkumar@uniphore.com>
2024-10-08 18:11:41 +0530

    add 1st terraform file

---
+++ terraform-code/v2-ec2.tf
@@ -0,0 +1,27 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-0fff1b9a61dec8a5f"
+  instance_type = "t2.micro"
+  key_name      = "dpp"
+  security_groups = [ "demo-sg" ]
+}
+
+resource "aws_security_group" "demo-sg" {
+  name        = "demo-sg"
+  description = "Allow SSH inbound rule"
+  tags = {
+    Name = "demosg"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "demo-sg-ingress" {
+  security_group_id = aws_security_group.demo-sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
