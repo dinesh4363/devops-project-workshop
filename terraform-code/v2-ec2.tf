b7aa654f705caadf930d685f35a72ef82b32c7ab
Dinesh Kumar <dineshkumar@uniphore.com>
2024-10-09 12:18:13 +0530

    modified v2-ec2

--- terraform-code/v2-ec2.tf
+++ terraform-code/v2-ec2.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "web" {
   ami           = "ami-0fff1b9a61dec8a5f"
   instance_type = "t2.micro"
   key_name      = "dpp"
-  security_groups = [ "demo-sg" ]
+  security_groups = ["demo-sg"]
 }
 
 resource "aws_security_group" "demo-sg" {
